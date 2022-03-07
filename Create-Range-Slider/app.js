428b982ad52df2c2f979df286a89398b66c7528d
Minh Truong <minh.truong@sapotacorp.vn>
2022-03-07 10:19:52 +0700

    add project create-range-slider

---
+++ Create-Range-Slider/app.js
@@ -0,0 +1,19 @@
+var range = document.querySelector('.range');
+var process = document.querySelector('.process');
+
+function update(percent) {
+    process.style.width = `${percent}%`;
+    value.innerHtml = `${percent}%`;
+}
+
+range.addEventListener('mousemove', function(e) {
+    var processWidth = e.pageX - this.offsetLeft;
+    var percent = processWidth / this.offsetWidth * 100;
+    percent = Math.round(percent);
+    update(percent);
+})
+
+var slider = document.getElementById('#slider');
+slider.addEventListener('change', function(val) {
+    
+})
